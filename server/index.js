60fc1692f1cc13a4b7d4cf485bdb01da2ac220ca
Melion147 <melionmails@gmail.com>
2022-08-13 18:13:21 +0200

    Basic MongoDB setup (Connection, env)

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
@@ -7,10 +8,17 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect('mongodb://XXXXX/XXXXXX', {
+mongoose.connect(`mongodb://melion:${process.env.MONGO_PASSKEY}@ac-vaxsrgl-shard-00-00.bbgmqhv.mongodb.net:27017,ac-vaxsrgl-shard-00-01.bbgmqhv.mongodb.net:27017,ac-vaxsrgl-shard-00-02.bbgmqhv.mongodb.net:27017/?ssl=true&replicaSet=atlas-evf9o0-shard-0&authSource=admin&retryWrites=true&w=majority`, {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true 
-}).then(() => console.log("Connected to MongoDB")).catch(console.error);
+});
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+	console.log("MongoDB database connection established successfully");
+})
 
 
-app.listen(3001)
+app.listen(process.env.INDEX_PORT, function() {
+	console.log("Server is running on Port: " + process.env.INDEX_PORT);
+});
