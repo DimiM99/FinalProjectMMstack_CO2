f09cb02099b9abb030df70669fab2591d06464a9
Melion147 <melionmails@gmail.com>
2022-08-13 19:17:43 +0200

    Basic MongoDB setup (Connection, env)

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom"
+import Login from "./containers/Login";
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <h1>Home</h1> } />
         <Route path="/xxxx" element={ <h1>XXXX</h1> } />
+        <Route path="/login" element={ <Login/> } />
       </Routes>
     </div>
   )
