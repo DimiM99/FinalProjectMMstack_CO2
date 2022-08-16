a23edeb6e7f2d9e6dfe71052b69dff8fb5cdbc7b
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2022-08-16 21:05:38 +0200

    fixed crash issue on Box/MUI

--- client/src/components/UserActions.js
+++ client/src/components/UserActions.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
 import SettingsIcon from '@mui/icons-material/Settings';
-import Box from "@mui/material";
+import {Box} from "@mui/material";
 
 export default function IconButtons() {
     return (
