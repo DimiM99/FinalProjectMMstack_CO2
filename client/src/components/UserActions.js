7c3593f78564c3fe89cbce396cf01c3824774dc5
Melion147 <melionmails@gmail.com>
2022-08-16 20:41:35 +0200

    Navigation refactor

--- client/src/components/UserActions.js
+++ client/src/components/UserActions.js
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
 import SettingsIcon from '@mui/icons-material/Settings';
+import HomeIcon from '@mui/icons-material/Home';
 import {Box} from "@mui/material";
+import {useLocation, useNavigate} from "react-router-dom";
 
 export default function IconButtons() {
+    let navigate = useNavigate()
+    let location = useLocation()
+    console.log(location)
     return (
         <Box marginTop='10px'>
-            <IconButton aria-label="Settings" onClick={ () => { window.location.href = "/settings"; } }>
-                <SettingsIcon/>
+            <IconButton aria-label="Settings" onClick={ () => location.pathname ===  "/settings" ? navigate("/") : navigate("/settings") }>
+                {!(location.pathname ===  "/settings") ? (<SettingsIcon/>) : (<HomeIcon/>)}
             </IconButton>
         </Box>
     );
