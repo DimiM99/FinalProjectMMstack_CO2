dbb039b847e3c241498384ff5c72a1d352c36bee
Mateusz Boratynski <melionmails@gmail.com>
2022-08-21 13:00:22 +0200

    User Register username Modal

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,18 @@ import {useEffect, useState} from "react";
 import { login } from "./apis/auth"
 import useUserStore from "./store/user";
 import Overview from "./views/Overview";
+import SetUsernameModal from "./components/SetUsernameModal";
 
 
 function App() {
     const user = useUserStore(state => {
         return {walletId: state.walletId, accessToken: state.accessToken, refreshToken: state.refreshToken, logoutRevocation: state.logoutRevocation}
     })
-    const {setUser,setUsername} = useUserStore()
+    const {setUser,setUsername, username} = useUserStore()
 
     const [walletId, setWalletId] = useState(null)
+    const [open, setOpen] = useState(false)
+    const [isUsernameSet, setIsUsernameSet] = useState(false)
     window.ethereum.on('accountsChanged', (accounts) => {
         if(!accounts.length){
             user.logoutRevocation()
@@ -24,17 +27,26 @@ function App() {
                 .then(({accessToken,refreshToken, username}) => {
                     window.localStorage.setItem("authTokens", JSON.stringify({accessToken, refreshToken}))
                     setUser(walletId,  refreshToken, accessToken)
-                    setUsername(username)
+                    if(username){
+                        setUsername(username)
+                        setIsUsernameSet(true)
+                    }else {
+                        setOpen(true)
+                    }
                 })
                 .catch(e=>console.log(e))
         }
     },[walletId])
+
   return (
     <div className="App">
-        {user.accessToken  ? (
+        {user.accessToken && isUsernameSet  ? (
             <Overview/>
         ): (
-            <Login setWalletId={setWalletId}/>
+            <>
+                <Login setWalletId={setWalletId}/>
+                <SetUsernameModal setIsUsernameSet={setIsUsernameSet} open={open} setOpen={setOpen}/>
+            </>
         )}
     </div>
   )
--- client/src/components/test.js
+++ client/src/components/test.js
@@ -1,23 +1,25 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import useUserStore from '../store/user'
 import useAxiosPrivate from "../hooks/axiosInterceptor";
 import axios from 'axios'
+import SetUsernameModal from "./SetUsernameModal";
 const Test = () => {
     const {updateAccessToken, accessToken, logoutRevocation} = useUserStore()
     console.log("accessToken : ",accessToken)
+    const [open, setOpen] = useState(false)
 
     const getData = async () =>{
         const response = await axios.get('http://localhost:3001/getsimpledata', {data: {n: "cool"}} );
         console.log(response)
     }
 
-
-
     return (
         <div>
             <button onClick={()=>updateAccessToken()}>refreshToken</button>
             <button onClick={getData}>test</button>
             <button onClick={logoutRevocation}>logout</button>
+            <button onClick={()=> setOpen(true)}>open Modal for username</button>
+            <SetUsernameModal open={open} setOpen={setOpen}/>
         </div>
     );
 };
